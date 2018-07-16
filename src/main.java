fc5ab1656f63550fa35a51d641016ce062fa95bf
Silird <Silird@gmail.com>
2018-07-16 23:30:03 +0300

    init

---
+++ src/main.java
@@ -0,0 +1,23 @@
+public class main {
+    public static void main(String[] args) {
+        String l = "\\";
+        String q = "\"";
+        String n = "n";
+        String p = " +";
+        String o = "                ";
+        String s = "public class main {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        String l = ;\n" +
+                "        String q = ;\n" +
+                "        String n = ;\n" +
+                "        String p = ;\n" +
+                "        String o = ;\n" +
+                "        String s = ;\n" +
+                "        System.out.println(s.substring(0, 84) + q + l + l + q + s.substring(84, 105) + q + l + q + q + s.substring(105, 126) + q + n + q + s.substring(126, 147) + q + p + q + s.substring(147, 168) + q + o + q + s.substring(168, 189) + q + s.substring(0, 19) + l + n + q + p + s.substring(19, 20) + o + q + s.substring(20, 64) + l + n + q + p + s.substring(64, 65) + o + q + s.substring(65, 85) + l + n + q + p + s.substring(85, 86) + o + q + s.substring(86, 106) + l + n + q + p + s.substring(106, 107) + o + q + s.substring(107, 127) + l + n + q + p + s.substring(127, 128) + o + q + s.substring(128, 148) + l + n + q + p + s.substring(148, 149) + o + q + s.substring(149, 169) + l + n + q + p + s.substring(169, 170) + o + q + s.substring(170, 190) + l + n + q + p + s.substring(190, 191) + o + q + s.substring(191, 1191) + l + n + q + p + s.substring(1191, 1192) + o + q + s.substring(1192, 1197) + l + n + q + p + s.substring(1197, 1198) + o + q + s.substring(1198, 1199) + q + s.substring(189));\n" +
+                "    }\n" +
+                "}";
+        System.out.println(s.substring(0, 84) + q + l + l + q + s.substring(84, 105) + q + l + q + q + s.substring(105, 126) + q + n + q + s.substring(126, 147) + q + p + q + s.substring(147, 168) + q + o + q + s.substring(168, 189) + q + s.substring(0, 19) + l + n + q + p + s.substring(19, 20) + o + q + s.substring(20, 64) + l + n + q + p + s.substring(64, 65) + o + q + s.substring(65, 85) + l + n + q + p + s.substring(85, 86) + o + q + s.substring(86, 106) + l + n + q + p + s.substring(106, 107) + o + q + s.substring(107, 127) + l + n + q + p + s.substring(127, 128) + o + q + s.substring(128, 148) + l + n + q + p + s.substring(148, 149) + o + q + s.substring(149, 169) + l + n + q + p + s.substring(169, 170) + o + q + s.substring(170, 190) + l + n + q + p + s.substring(190, 191) + o + q + s.substring(191, 1191) + l + n + q + p + s.substring(1191, 1192) + o + q + s.substring(1192, 1197) + l + n + q + p + s.substring(1197, 1198) + o + q + s.substring(1198, 1199) + q + s.substring(189));
+    }
+}
+
+
